2adcd93c05641433113faf2add71e3edea9c9de0
shinyting <zhangting20.90@163.com>
2015-12-16 16:22:37 +0800

    add consolidate&&swig module and views folder

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var cons = require('consolidate');
+var app = express();
+
+// app.get('/', function(req, res) {
+// 	res.render('index');
+// 	res.send('Hello World! world world world');
+// });
+
+app.engine('html', cons.swig);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		title: 'apple'
+	});
+});
+
+var server = app.listen(8000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
+
+app.use(express.static('public'));
