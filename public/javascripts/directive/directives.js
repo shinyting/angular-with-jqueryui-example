7dd88f4910b38a9e82f31e76b9f9041ddfc6a5e5
shinyting <zhangting20.90@163.com>
2016-01-04 14:41:52 +0800

    modify drag directive

--- public/javascripts/directive/directives.js
+++ public/javascripts/directive/directives.js
@@ -2,8 +2,23 @@ ajudemo.directive("dragElement", function () {
 	return {
 		scope: {},
 		link: function (scope, element, attrs) {
+			console.log(attrs);
 			element.draggable({
-				addClasses: false
+				addClasses: false,
+				cursor: "pointer", //拖动时鼠标的形状
+				cursorAt: { //拖动时鼠标在被拖动元素的位置
+					left: 5,
+					top: 10
+				},
+				delay: 30, //延时3秒开始拖动
+				// disabled: true //不可拖动
+				grid: [40,10],
+				start: function (event, ui) {
+					console.log("drag start");
+				},
+				drag: function (event, ui) {
+					ui.position.left = Math.min(100, ui.position.left);
+				}
 			})
 		}
 	}
